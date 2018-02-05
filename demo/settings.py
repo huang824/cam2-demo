9f87d3bcd54d3f4ec6daba0a6804cd501458ad46
Weiqing Huang <huang824@purdue.edu>
2018-02-04 21:54:24 -0500

    changed settings again

--- demo/settings.py
+++ demo/settings.py
@@ -118,7 +118,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
